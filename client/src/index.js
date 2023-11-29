7323cca4376d89526905762197d7059c142da13b
OSSwebmaster2003 <sodikovotabeksobirjonovich1509@gmail.com>
2023-11-29 13:06:07 -0800

    set up of data , redux toolkit , colors, mui dark/light mode

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { configureStore } from "@reduxjs/toolkit";
+import { globalSlice } from "state";
+import { Provider } from "react-redux";
 import App from "./App";
 import "./index.css";
+
+// store
+export const store = configureStore({
+  reducer: {
+    global: globalSlice,
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
